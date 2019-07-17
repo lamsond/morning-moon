8328c327c725c855bb48ebb087671c6a859ab548
conrad <lamsond@wctech.org>
2019-07-16 20:18:50 -0400

    set-up typography

--- lamsite/src/pages/index.js
+++ lamsite/src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+<div>
+    <h1>Coming Soon...Lamsite</h1>
+    <p>A site to share course materials, assignments,
+        coding projects, showcase achievements, post lesson plans,
+        and blog about math, coding, and baseball.
+    </p>
+    <code>const x = Math.sqrt(2);</code>
+</div>
+);
