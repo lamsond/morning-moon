536789c0880c8b3e5713af1514ae688727c0754c
conrad <lamsond@wctech.org>
2019-07-18 21:54:00 -0400

    added props to nugget

--- lamsite/src/components/lessonNugget.js
+++ lamsite/src/components/lessonNugget.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-export default () => {
+export default (props) => {
     return (
         <div>
-            <h5>Sept 3, 2019 <span>Algebra 1</span></h5>
-            <h2>Solving Quadratic Equations</h2>
+            <h5>{ props.date } - <span>{ props.subject }</span></h5>
+            <h2>{ props.title }</h2>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing
-                 elit, sed do eiusmod tempor incididunt ut labore
-                  et dolore magna aliqua. Ut enim ad minim veniam,
-                   quis nostrud exercitation ullamco laboris nisi
-                    ut aliquip ex ea commodo consequat.
+                { props.text }
             </p>
             <h5><Link to='/'>Read</Link></h5>
         </div>
     );
-}
+} 
--- lamsite/src/pages/index.js
+++ lamsite/src/pages/index.js
@@ -5,6 +5,20 @@ import LessonNugget from '../components/lessonNugget.js';
 export default () => (
 <Layout>
     <h1>Lessons</h1>
-    <LessonNugget />
+    <LessonNugget date='Sept 3, 2019' subject='Algebra 1' title='Graphing Parabolas' 
+        text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+         ullamco laboris nisi ut aliquip ex ea commodo consequat.' 
+    />
+    <LessonNugget date='Sept 3, 2019' subject='AP Computer Science Principles' title='HTML tags' 
+        text='Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+         pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+          anim id est laborum.' 
+    />
+    <LessonNugget date='Sept 3, 2019' subject='AP Computer Science A' title='Variables' 
+        text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+         ullamco laboris nisi ut aliquip ex ea commodo consequat.' 
+    />
 </Layout>
 );
