61f46f5464871f77489aed9ac78789e616bae69e
conrad <lamsond@wctech.org>
2019-07-22 17:57:00 -0400

    created subject folders for md files

--- lamsite/src/pages/index.js
+++ lamsite/src/pages/index.js
@@ -4,20 +4,23 @@ import { graphql } from 'gatsby';
 import Layout from "../components/layout.js";
 import LessonNugget from '../components/lessonNugget.js';
 
-export default ({ data }) => (
-<Layout>
-    <h1>{ data.allMarkdownRemark.totalCount } Lessons</h1>
-    {data.allMarkdownRemark.edges.map(({ node }) => (
-        <LessonNugget 
-            key={node.id}
-            date={node.frontmatter.date}
-            subject={node.frontmatter.subject}
-            title={node.frontmatter.title}
-            text={node.excerpt} 
-        />
-    ))}
-</Layout>
-);
+export default ({ data }) => {
+    console.log(data);
+    return (
+    <Layout>
+        <h1>{ data.allMarkdownRemark.totalCount } Lessons</h1>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+            <LessonNugget 
+                key={node.id}
+                date={node.frontmatter.date}
+                subject={node.frontmatter.subject}
+                title={node.frontmatter.title}
+                text={node.excerpt} 
+            />
+        ))}
+    </Layout>
+    );
+}
 
 export const query = graphql`
 query {
