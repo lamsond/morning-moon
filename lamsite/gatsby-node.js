bd98016643ee8ac9c76e5d463f6170da6bdc5625
conrad <lamsond@wctech.org>
2019-07-23 14:53:20 -0400

    automated lesson page creation

--- lamsite/gatsby-node.js
+++ lamsite/gatsby-node.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const { createFilePath } = require('gatsby-source-filesystem');
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
@@ -14,6 +15,7 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 }
 
 exports.createPages = ({ graphql, actions }) => {
+    const { createPage } = actions;
     return graphql(`
         {
             allMarkdownRemark{
@@ -27,6 +29,15 @@ exports.createPages = ({ graphql, actions }) => {
             }
         }
     `).then(result => {
-        console.log(JSON.stringify(result, null, 4));
+        //console.log(JSON.stringify(result, null, 4));
+        result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+            createPage({
+                path: node.fields.slug,
+                component: path.resolve('./src/templates/lesson.js'),
+                context: {
+                    slug: node.fields.slug,
+                },
+            });
+        });
     });
 }
---
+++ lamsite/src/templates/lesson.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Layout from '../components/layout.js';
+
+export default () => {
+    return (
+        <Layout>
+            <div>Hello lesson</div>
+        </Layout>
+    )
+}
