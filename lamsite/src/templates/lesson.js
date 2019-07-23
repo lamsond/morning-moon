118723ed7af9f2993465bd614ae9a475aed1e575
conrad <lamsond@wctech.org>
2019-07-23 15:04:49 -0400

    include inner HTML in template

--- lamsite/src/templates/lesson.js
+++ lamsite/src/templates/lesson.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
 import Layout from '../components/layout.js';
 
-export default () => {
+export default ({ data }) => {
+    const lesson = data.markdownRemark;
     return (
         <Layout>
-            <div>Hello lesson</div>
+            <div>
+                <h1>{ lesson.frontmatter.title }</h1>
+                <div dangerouslySetInnerHTML={{ __html: lesson.html }} />
+            </div>
         </Layout>
-    )
+    );
 }
+
+export const query = graphql`
+    query($slug: String!){
+        markdownRemark(fields: {slug: {eq: $slug}}){
+            html
+            frontmatter{
+                title
+            }
+        }
+    }
+`
