605b4125ef044fd60450a0dd8b86acebb4983194
conrad <lamsond@wctech.org>
2019-07-23 14:32:36 -0400

    create slugs

--- lamsite/gatsby-node.js
+++ lamsite/gatsby-node.js
@@ -1,6 +1,32 @@
-exports.onCreateNode = ({ node, getNode }) => {
+const { createFilePath } = require('gatsby-source-filesystem');
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+    const { createNodeField } = actions;
+
     if(node.internal.type === 'MarkdownRemark'){
-        const fileNode = getNode(node.parent);
-        console.log('\n', fileNode.relativePath);
+        const slug = createFilePath({ node, getNode, basePath: 'pages '});
+        createNodeField({
+            node,
+            name: 'slug',
+            value: slug,
+        });
     }
+}
+
+exports.createPages = ({ graphql, actions }) => {
+    return graphql(`
+        {
+            allMarkdownRemark{
+                edges{
+                    node{
+                        fields{
+                            slug
+                        }
+                    }
+                }
+            }
+        }
+    `).then(result => {
+        console.log(JSON.stringify(result, null, 4));
+    });
 }
