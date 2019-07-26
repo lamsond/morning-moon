153fa0991a13256759bf4c4386c1349a61201faf
conrad <lamsond@wctech.org>
2019-07-25 23:01:55 -0400

    added delta lambda letters as a temp logo

--- lamsite/src/components/top.js
+++ lamsite/src/components/top.js
@@ -7,7 +7,7 @@ export default () => {
     return (
         <div className={topStyles.top}>
             <ul  className={topStyles.list}>
-            <li><h5><Link to='/'>main</Link></h5></li>
+            <li><h5><Link to='/'><span className={topStyles.logo}>&#x0394;&#x03bb;</span></Link></h5></li>
                 <li><h5><Link to='/courses/'>courses</Link></h5></li>
                 <li><h5><Link to='/showcase/'>showcase</Link></h5></li>
                 <li><h5><Link to='/blog/'>blog</Link></h5></li>
