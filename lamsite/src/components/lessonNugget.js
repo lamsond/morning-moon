10d751126d9cb6e075991b53947c94872dac8aca
conrad <lamsond@wctech.org>
2019-08-18 18:51:32 -0400

    fixed READ margins on lessonNugget

--- lamsite/src/components/lessonNugget.js
+++ lamsite/src/components/lessonNugget.js
@@ -7,10 +7,10 @@ export default (props) => {
         <div>
             <h5>{ props.date } - <span>{ props.subject }</span>: <span>{ props.unit }</span></h5>
             <Link to={ props.slug }><h2>{ props.title }</h2></Link>
-            <p>
+            <p className={lessonNuggetStyles.excerpt}>
                 { props.excerpt }
             </p>
-            <h5><Link to={ props.slug } className={ lessonNuggetStyles.read }>Read More</Link></h5>
+            <h5><Link to={ props.slug } className={ lessonNuggetStyles.read }>Read</Link></h5>
             <p>
                 <h5 className={lessonNuggetStyles.hw_label}>HOMEWORK:&nbsp;</h5>
                 { props.homework } 
